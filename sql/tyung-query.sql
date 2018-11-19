9b88aadfce2757a92a1eb73ffee51cc19194301f
scott <tyung@dons.usfca.edu>
2018-11-19 14:12:56 -0800

    remaned sql files" ;

--- sql/tyung-query.sql
+++ sql/tyung-query.sql
@@ -6,6 +6,7 @@ select count(year) from movies;
 select count(year) from movies where year = 0;
 select count(year) from movies where year > 1500;
 select year, count(*) from movies group by year;
+select (year%100)/10, count(*) from movies, group by (year%100)/10;
 select genre, count (*) from has_genre group by genre;
 select count(*) from ratings group by rating;
 select count(distinct movieid) from movies where not exists (select movieid from tags group by tags.movieid having tags.movieid = movies.movieid);
