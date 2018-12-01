1fed8b2f084662135d47c17f97dc11d72e703b59
scott <tyung@dons.usfca.edu>
2018-11-30 23:09:27 -0800

    modified both query files

--- sql/tyung-query-part2.sql
+++ sql/tyung-query-part2.sql
@@ -8,7 +8,7 @@ select count(year) from movies where year > 1500;
 select year, count(*) from movies group by year;
 select (year%100)/10, count(*) from movies group by (year%100)/10;
 select genre, count (*) from has_genre group by genre;
-select count(*) from ratings group by rating;
+select rating, count(*) from ratings group by rating;
 select count(distinct movieid) from movies where not exists (select movieid from tags group by tags.movieid having tags.movieid = movies.movieid);
 select count(distinct movieid) from movies natural join tags where not exists (select movieid from ratings group by ratings.movieid having ratings.movieid = movies.movieid
 );
